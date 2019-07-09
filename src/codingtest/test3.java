967be9c8a047b2a90d0d2bde80728624fcd5bb2b
choiwoobin <wb1762@gmail.com>
2019-07-09 18:24:51 +0900

    coding

--- src/codingtest/test3.java
+++ src/codingtest/test3.java
@@ -1,5 +1,40 @@
 package codingtest;
 
-public class test3 {
+import java.util.*;
+class test3 {
+  public String solution(int a, int b) {
+       String answer = "";
+      Calendar cal = Calendar.getInstance();
+       cal.set(Calendar.YEAR,Integer.parseInt("2016"));
+       cal.set(Calendar.MONTH,a-1);
+       cal.set(Calendar.DATE,b);
+      
+   switch(cal.get(Calendar.DAY_OF_WEEK)){
+       case 1: 
+            answer ="SUN";
+            break;
+           
+       case 2:
+            answer ="MON";
+           break;
+       case 3: 
+            answer ="TUE";
+            break;
+        case 4: 
+            answer ="WED";
+            break;
+        case 5: 
+            answer ="THU";
+            break;
+        case 6: 
+            answer ="FRI";
+            break;
+        case 7: 
+            answer ="SAT";
+            break;
+   }
 
+    
+      return answer;
+  }
 }
---
+++ src/codingtest/test4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+package codingtest;
+
+
+class test4 {
+  public String solution(String s) {
+       String answer;
+      char[] tmp=s.toCharArray();
+      if(s.length()%2==1)
+          answer=Character.toString(tmp[tmp.length/2]);
+    
+      else
+          answer=Character.toString(tmp[tmp.length/2-1]).concat(Character.toString(tmp[tmp.length/2]));
+       return answer;
+  }
+}
